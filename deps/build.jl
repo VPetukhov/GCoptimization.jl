3c7818147ee5a12ad8b95cb4b7fc5150c47f8633
gnimuc <gnimuckey@gmail.com>
2016-09-12 01:23:42 +0800

    fix windows bugs

--- deps/build.jl
+++ deps/build.jl
@@ -8,33 +8,40 @@ prefix = joinpath(dirname(@__FILE__), "usr")
 srcdir = joinpath(dirname(@__FILE__), "src", "$name-$version")
 builddir = joinpath(dirname(@__FILE__), "builds", "$name-$version")
 
-# download source code
-mkpath("downloads")
-tarball = "downloads/$name-$version.tar.gz"
-if !isfile(tarball)
-    info("Downloading $name-$version source from $repo...")
-    download("$repo/archive/v$version.tar.gz", tarball)
-end
-
-# unpack source tarball
-mkpath(srcdir)
-run(`tar xzf $tarball -C src`)
-
-# copy header files
-mkpath(joinpath(prefix, "include"))
-cp(joinpath(srcdir, "include"), joinpath(prefix, "include"), remove_destination=true)
-
-
 if is_windows()
-    const archive = "downloads/$name-$version.zip"
+    # download source code
+    mkpath("downloads")
+    source = "downloads/$name-$version.zip"
+    if !isfile(source)
+        info("Downloading $name-$version source from $repo...")
+        download("$repo/archive/v$version.zip", source)
+    end
+
+    # unpack source zip
+    mkpath(srcdir)
+    run(`7z x $source -osrc -y`)
+
+    const archive = "downloads/$libname-$suffix-$version.zip"
     if !isfile(archive)
         info("Downloading $name-$version binaries from $repo...")
         download("$repo/releases/download/v$version/$libname-$suffix.zip", archive)
     end
-    run(`7z e $archive`)
+    run(`7z x $archive -y`)
 end
 
 if is_apple() || is_linux()
+    # download source code
+    mkpath("downloads")
+    source = "downloads/$name-$version.tar.gz"
+    if !isfile(source)
+        info("Downloading $name-$version source from $repo...")
+        download("$repo/archive/v$version.tar.gz", source)
+    end
+
+    # unpack source tarball
+    mkpath(srcdir)
+    run(`tar xzf $source -C src`)
+
     try
         mkpath("$prefix/lib")
         mkpath(builddir)
@@ -48,3 +55,7 @@ if is_apple() || is_linux()
         download("$repo/releases/download/v$version/$libname.$(Libdl.dlext)", "$prefix/lib/$libname.$(Libdl.dlext)")
     end
 end
+
+# copy header files
+mkpath(joinpath(prefix, "include"))
+cp(joinpath(srcdir, "include"), joinpath(prefix, "include"), remove_destination=true)
